9a97523bb68a8b3182ff820767314811584435f0
Christian Stough <cstough97@gmail.com>
2024-04-15 14:36:59 -0500

    build improvments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,27 @@
 import Link from "next/link";
+import { mock } from "node:test";
+
+const mockUrls = [
+  "https://utfs.io/f/39f40ebb-e265-4a30-8fe4-7dfc118412bf-zcuagf.png",
+  "https://utfs.io/f/f7237ba6-ea54-40e6-a650-86b8a35c8a44-bx2mpy.png",
+  "https://utfs.io/f/3f5a1c13-7ab1-4209-8fa2-a24fdec12482-qz53fz.png",
+  "https://utfs.io/f/7e02dd8d-ea93-4abe-9414-29d7788eaf1b-ir56qd.png",
+  "https://utfs.io/f/6feee506-94f1-4485-b90e-96b1388fa8f7-1v0koq.png",
+  "https://utfs.io/f/2a6ed1e0-aac5-40b7-9e6a-21e5ad986ce8-zb6xfr.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
+    <main>
+      <div className="flex flex-wrap gap-2">
+        {[...mockImages, ...mockImages].map((image) => (
+          <img key={image.id} src={image.url} alt="mock" className="w-1/4" />
+        ))}
       </div>
     </main>
   );
